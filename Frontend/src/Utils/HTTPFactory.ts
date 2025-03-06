e88761f13b98719c77921dbbcd8f6d1cef3ea82e
Kovacs_Roland_Gergo <kovaxrawland@mailbox.unideb.hu>
2025-03-06 13:57:59 +0100

    feat: minor changes

---
+++ Frontend/src/Utils/HTTPFactory.ts
@@ -0,0 +1,18 @@
+
+import http from "http";
+
+function getFactory(url:string){
+
+}
+
+function updateFactory(){
+
+}
+
+function createFactory(){
+
+}
+
+function deleteFactory(){
+
+}
--- src/Types/category.type.ts
+++
@@ -1,8 +0,0 @@
-
-interface ICategory extends Document {
-    name: string;
-    description: string;
-    image: string;
-}
-
-export {ICategory};
